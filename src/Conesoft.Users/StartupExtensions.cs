090fc5e71bdabe4e3090265d25d1dca5c7fa60ec
David Spörri <david.spoerri@coop.ch>
2020-02-08 18:57:26 +0100

    version 1

---
+++ src/Conesoft.Users/StartupExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Conesoft.Users
+{
+    public static class StartupExtensions
+    {
+        public static void AddUsers(this IServiceCollection services, string rootPath = "")
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromDays(365);
+                options.SlidingExpiration = true;
+            });
+
+            UserController.RootPath = rootPath;
+        }
+
+        public static void UseUsers(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+        }
+    }
+}
---
+++ src/Conesoft.Users/UserController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Conesoft.Users
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        internal static string RootPath { get; set; }
+
+        string UserFile(string username) => RootPath != "" ? System.IO.Path.Combine(RootPath, username + ".txt") : username + ".txt";
+
+        [HttpPost("login")]
+        public async Task<IActionResult> PostLoginAsync(string username, string password, string redirectto)
+        {
+            var passwordHasher = new PasswordHasher<string>();
+
+            if (System.IO.File.Exists(UserFile(username)))
+            {
+                var lines = await System.IO.File.ReadAllLinesAsync(UserFile(username));
+
+                var salt = lines.First();
+                var hashed = lines.Last();
+
+                var validLogin = passwordHasher.VerifyHashedPassword(username, hashed, password + salt);
+
+                if (validLogin == PasswordVerificationResult.Success)
+                {
+                    // Create the identity from the user info
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, username));
+
+                    // Authenticate using the identity
+                    var principal = new ClaimsPrincipal(identity);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
+
+                }
+            }
+
+            return Redirect(redirectto);
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> PostLogoutAsync(string redirectto)
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect(redirectto);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> PostRegisterAsync(string username, string password, string redirectto)
+        {
+            var passwordHasher = new PasswordHasher<string>();
+
+            if (System.IO.File.Exists(UserFile(username)) == false)
+            {
+                var newsalt = Guid.NewGuid().ToString().ToLower().Replace("-", "");
+                await System.IO.File.WriteAllLinesAsync(UserFile(username), new[] {
+                    newsalt,
+                    passwordHasher.HashPassword(username, password + newsalt)
+                });
+            }
+
+            // Create the identity from the user info
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+            identity.AddClaim(new Claim(ClaimTypes.Name, username));
+
+            // Authenticate using the identity
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
+
+            return Redirect(redirectto);
+        }
+    }
+}
