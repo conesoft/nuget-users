1c24b6b7f4870065e8e635043204e71dae2aeed4
David Spörri <davepermen@hotmail.com>
2020-02-16 21:02:41 +0100

    ....

--- src/Conesoft.Users/StartupExtensions.cs
+++ src/Conesoft.Users/StartupExtensions.cs
@@ -17,7 +17,6 @@ namespace Conesoft.Users
             })
             .AddCookie(options =>
             {
-                options.Cookie.Expiration = TimeSpan.FromDays(365);
                 options.ExpireTimeSpan = TimeSpan.FromDays(365);
                 options.SlidingExpiration = true;
             });
