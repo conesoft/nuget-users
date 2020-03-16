2f95f2bca62e9f35ba85c7ba2bb56da350801f85
David Spörri <davepermen@hotmail.com>
2020-03-16 11:09:21 +0100

    Update UserController.cs

--- src/Conesoft.Users/UserController.cs
+++ src/Conesoft.Users/UserController.cs
@@ -73,17 +73,16 @@ namespace Conesoft.Users
                     newsalt,
                     passwordHasher.HashPassword(username, password + newsalt)
                 });
+
+                // Create the identity from the user info
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+                identity.AddClaim(new Claim(ClaimTypes.Name, username));
+
+                // Authenticate using the identity
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
             }
-
-            // Create the identity from the user info
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
-            identity.AddClaim(new Claim(ClaimTypes.Name, username));
-
-            // Authenticate using the identity
-            var principal = new ClaimsPrincipal(identity);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = true });
-
             return Redirect(redirectto);
         }
     }
