324e106dcc4d1a4a2bf6a5e3228ccf47147e5316
David Spörri <davepermen@hotmail.com>
2023-01-16 22:29:30 +0100

    fixed... (breaks old stuff but that's ok)

--- src/UserExtensions.cs
+++ src/UserExtensions.cs
@@ -12,42 +12,59 @@ public static class UserExtensions
         options.Cookie.Name = cookiename;
         options.ExpireTimeSpan = TimeSpan.FromDays(365);
         options.SlidingExpiration = true;
-        options.ReturnUrlParameter = "returnto";
+        options.ReturnUrlParameter = "redirectto";
         options.LoginPath = "/user/login";
         options.LogoutPath = "/user/logout";
     });
 
-    private static bool VerifyPersistedAccount(string user, string password)
+    private static async Task<ClaimsPrincipal?> FindVerifiedAccount(string username, string password, bool createifneeded)
     {
         PasswordHasher<string> passwordHasher = new();
-        if (File.Exists(Path.Combine(directory, user + ".txt")))
+        var userfilepath = Path.Combine(directory, username + ".txt");
+
+        if (File.Exists(userfilepath) == false && createifneeded == false)
         {
-            var lines = File.ReadAllLines(Path.Combine(directory, user + ".txt"));
-            var salt = lines.First();
-            var hashed = lines.Last();
-            return passwordHasher.VerifyHashedPassword(user, hashed, password + salt) == PasswordVerificationResult.Success;
+            return null;
         }
-        else
+        else if (File.Exists(userfilepath) == false)
         {
-            var salt = Guid.NewGuid().ToString().ToLower().Replace("-", "");
-            File.WriteAllLines(Path.Combine(directory, user + ".txt"), new[] { salt, passwordHasher.HashPassword(user, password + salt) });
-            return true;
+            var newsalt = Guid.NewGuid().ToString().ToLower().Replace("-", "");
+            await File.WriteAllLinesAsync(userfilepath, new[] { newsalt, passwordHasher.HashPassword(username, password + newsalt) });
         }
+
+        var lines = await File.ReadAllLinesAsync(userfilepath);
+        var salt = lines.First();
+        var hashed = lines.Skip(1).First();
+        var roles = lines.Skip(2).ToArray();
+
+        if (passwordHasher.VerifyHashedPassword(username, hashed, password + salt) != PasswordVerificationResult.Success)
+        {
+            return null;
+        }
+
+        return Principal.From(roles
+            .Select(r => new Claim(ClaimTypes.Role, r))
+            .Append(new Claim(ClaimTypes.Name, username))
+            .ToArray()
+        );
     }
 
     public static void MapUsers(this WebApplication app)
     {
         app.UseAuthentication();
 
-        app.MapPost("/user/login", (HttpContext context) =>
+        app.MapPost("/user/login", async (HttpContext context) =>
         {
             var login = context.GetLoginForm();
-            return VerifyPersistedAccount(login.Username, login.Password)
-                ? SignInAndRedirect(Principal.From(login.Username), new() { IsPersistent = true }, cadas, login.RedirectTo)
-                : Results.Unauthorized();
+            return await FindVerifiedAccount(login.Username, login.Password, createifneeded: true) switch
+            {
+                var user when user is not null => SignInAndRedirect(user, new() { IsPersistent = true }, cadas, login.RedirectTo),
+                _ => Results.Unauthorized()
+            };
         });
 
-        app.MapPost("/user/logout", (HttpContext context) => {
+        app.MapPost("/user/logout", (HttpContext context) =>
+        {
             var logout = context.GetLogoutForm();
             return SignOutAndRedirect(logout.RedirectTo);
         });
