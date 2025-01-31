0334c89a9f62bf3f2edc6c1741dccc306d2ff3a0
David Spörri <davepermen@hotmail.com>
2025-01-31 10:43:05 +0100

    using local referer for redirect

--- src/Content/Forms/FormExtensions.cs
+++ src/Content/Forms/FormExtensions.cs
@@ -6,7 +6,7 @@ static class FormExtensions
     public static LoginForm GetLoginForm(this HttpContext context) => new(
         context.Request.Form["username"].FirstOrDefault() ?? "",
         context.Request.Form["password"].FirstOrDefault() ?? "",
-        context.Request.Form["redirectto"].FirstOrDefault() ?? ""
+        context.Request.Form["redirectto"].FirstOrDefault() ?? context.GetLocalReferrer()
     );
 
     public static PasswordChangeForm? GetPasswordChangeForm(this HttpContext context) =>
@@ -14,10 +14,19 @@ static class FormExtensions
             context.User.Identity.Name!,
             context.Request.Form["current-password"].FirstOrDefault() ?? "",
             context.Request.Form["new-password"].FirstOrDefault() ?? "",
-            context.Request.Form["redirectto"].FirstOrDefault() ?? ""
+            context.Request.Form["redirectto"].FirstOrDefault() ?? context.GetLocalReferrer()
         ) : null;
 
     public static LogoutForm GetLogoutForm(this HttpContext context) => new(
-        context.Request.Form["redirectto"].FirstOrDefault() ?? ""
+        context.Request.Form["redirectto"].FirstOrDefault() ?? context.GetLocalReferrer()
     );
+
+    private static string GetLocalReferrer(this HttpContext context)
+    {
+        if(context.Request.GetTypedHeaders().Referer is Uri referer && context.Request.Host.Host == referer.Host)
+        {
+            return referer.PathAndQuery;
+        };
+        return "";
+    }
 }
